3b31dd26b04660671a6361b59f0f005030c17862
Clau <cjacome@lucasian.com>
2017-01-23 16:02:48 -0500

    Operaciones
    Signed-off-by: Clau <cjacome@lucasian.com>

---
+++ workspace/world/src/worldF/Sumar.java
@@ -0,0 +1,18 @@
+package worldF;
+
+public class Sumar {
+	
+	float x,i;
+	
+	public Sumar(float x, float i)
+	{
+		super();
+		this.x=x;
+		this.i=i;
+	}
+public void suma()
+{
+	System.out.println(x+i);
+
+}
+}
