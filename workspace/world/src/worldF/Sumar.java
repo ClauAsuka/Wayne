31d351f6b338526f5b47ea97ac697f9c5c12e709
Clau <cjacome@lucasian.com>
2017-01-23 16:31:25 -0500

    change 2
    Signed-off-by: Clau <cjacome@lucasian.com>

--- workspace/world/src/worldF/Sumar.java
+++ workspace/world/src/worldF/Sumar.java
@@ -7,7 +7,9 @@ public class Sumar {
 	public Sumar(float x, float y)
 	{
 		super();
+		// guarda el primer número en x
 		this.x=x;
+		// guarda el segundo núemero en y
 		this.y=y;
 	}
 public void suma()
