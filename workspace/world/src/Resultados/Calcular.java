d0f738f73ba81e734717440d7ed6912aa79cae5f
Clau <cjacome@lucasian.com>
2017-01-24 09:40:48 -0500

    pack
    Signed-off-by: Clau <cjacome@lucasian.com>

--- workspace/world/src/Resultados/Calcular.java
+++ workspace/world/src/Resultados/Calcular.java
@@ -6,7 +6,9 @@ public class Calcular extends IntroducirNumeros{
 	{
 		IntroducirNumeros in=new IntroducirNumeros();
 		
+		System.out.println("Suma");		
 		new Sumar(in.x,in.i).suma();
+		System.out.println("Resta");		
 		new Restar(in.x,in.i).resta();
 		new Dividir(in.x,in.i).divide();
 		new Multiplicar (in.x,in.i).multiplica();
