04c8f4f0c2d6f83e747e2a48213a65d69267a308
Clau <cjacome@lucasian.com>
2017-01-25 14:44:25 -0500

    nuevo modelo
    Signed-off-by: Clau <cjacome@lucasian.com>

--- workspace/world/src/Resultados/Calcular.java
+++ workspace/world/src/Resultados/Calcular.java
@@ -6,11 +6,13 @@ public class Calcular extends IntroducirNumeros{
 	{
 		IntroducirNumeros in=new IntroducirNumeros();
 		
-		System.out.println("Suma");		
+		System.out.println("Suma:");		
 		new Sumar(in.x,in.i).suma();
-		System.out.println("Resta");		
+		System.out.println("Resta:");		
 		new Restar(in.x,in.i).resta();
+		System.out.println("División:");
 		new Dividir(in.x,in.i).divide();
+		System.out.println("Multiplicación:");
 		new Multiplicar (in.x,in.i).multiplica();
 		
 	}
