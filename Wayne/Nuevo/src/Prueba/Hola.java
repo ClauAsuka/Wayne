b56a58ed3fd718025bdda96e6bf3b6c4d283c755
Clau <cjacome@lucasian.com>
2017-01-30 11:10:28 -0500

    Cambios rama 1
    Signed-off-by: Clau <cjacome@lucasian.com>

---
+++ Wayne/Nuevo/src/Prueba/Hola.java
@@ -0,0 +1,8 @@
+package Prueba;
+
+public class Hola {
+	public static void main(String[] argumentos){
+		System.out.println("Hola mundo");
+		System.out.println("Hola mundo de la rama1");
+						}
+}
