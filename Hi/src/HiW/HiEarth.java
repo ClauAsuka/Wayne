035d0de0472e5019bf60ee43fad83a45a4c1978a
Clau <cjacome@lucasian.com>
2017-01-31 10:14:37 -0500

    Versión 3.0
    Signed-off-by: Clau <cjacome@lucasian.com>

--- Hi/src/HiW/HiEarth.java
+++ Hi/src/HiW/HiEarth.java
@@ -5,14 +5,13 @@ public class HiEarth {
 	public static void main(String[] args) 
 	{
 			System.out.println("Hola mundo");
-			System.out.println("salut monde");
+			System.out.println("salut monde");			
+			System.out.println("Hallo Welt");
+			System.out.println("Hi world");
+			
 			System.out.println("Hola secretaria");
 			System.out.println("salut secrétaire");
-			System.out.println("Hallo Welt");
-			System.out.println("Hola secretaria");
 			System.out.println("Hallo Sekretärin");
-			System.out.println("Hi world");
-			System.out.println("Hola secretaria");
 			System.out.println("Hi secretary");			
 	}
 }
