1cbdbcc229749c46d962e319f04d38e225d08064
Clau <cjacome@lucasian.com>
2017-01-31 15:40:39 -0500

    Signed-off-by: Clau <cjacome@lucasian.com>

--- Hi/src/HiW/HiEarth.java
+++ Hi/src/HiW/HiEarth.java
@@ -15,9 +15,8 @@ public class HiEarth {
 			System.out.println("Hi secretary");
 			System.out.println(" ");
 			System.out.println("Hola jefe");
-<<<<<<< HEAD
 			System.out.println("Hola Boss");
-			System.out.println("Hallo Kopf");2
+			System.out.println("Hallo Kopf");
 			System.out.println("salut chef");
 	}
 }
